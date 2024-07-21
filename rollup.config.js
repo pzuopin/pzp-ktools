a0018c11211870bc25900e4c5453e24706de9668
pzuopin <pzuopin@163.com>
2024-07-21 14:16:10 +0800

    publish

--- rollup.config.js
+++ rollup.config.js
@@ -4,31 +4,31 @@ import typescript from 'rollup-plugin-typescript';
 import pkg from './package.json';
 
 export default [
-    // UMD for browser-friendly build
-    {
-      input: 'src/index.ts',
-      output: {
-        name: 'pzp-ktools',
-        file: pkg.browser,
-        format: 'umd',
-        exports: 'auto'
-      },
-      plugins: [
-        resolve(),
-        commonjs(),
-        typescript()
-      ]
+  // UMD for browser-friendly build
+  {
+    input: 'src/index.ts',
+    output: {
+      name: 'pzp-ktools',
+			file: pkg.browser,
+      format: 'umd',
+      exports: 'auto'
     },
-    // CommonJS for Node and ES module for bundlers build
-    {
-      input: 'src/index.ts',
-      external: ['ms'],
-      plugins: [
-        typescript()
-      ],
-      output: [
-        {  file: pkg.main, format: 'cjs', exports: 'auto' },
-        {  file: pkg.module, format: 'es', exports: 'auto' }
-      ]
-    }
-]
+    plugins: [
+      resolve(),
+      commonjs(),
+      typescript()
+    ]
+  },
+  // CommonJS for Node and ES module for bundlers build
+  {
+    input: 'src/index.ts',
+    external: ['ms'],
+    plugins: [
+      typescript()
+    ],
+    output: [
+      {  file: pkg.main, format: 'cjs', exports: 'auto' },
+      {  file: pkg.module, format: 'es', exports: 'auto' }
+    ]
+  }
+];
